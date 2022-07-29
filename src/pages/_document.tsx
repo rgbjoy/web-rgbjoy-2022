c1f76439adcc1f5d479e255c469580975c5bc5ab
Tom Fletcher <rgbjoy@users.noreply.github.com>
2022-07-29 09:59:44 -0400

    Add GA and footer tweaks
    • Created GA tag
    • Add footer styling tweaks
    • Actually write better commit comments

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,23 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
+
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `,
+            }}
+          />
       </Head>
       <body>
         <Main />
