f63c40240e6c9da4fc9fa26fc7408993f22ac369
Tom Fletcher <rgbjoy@users.noreply.github.com>
2022-07-31 21:52:12 -0400

    404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head"
+import Link from "next/link"
+import Layout from "../components/Layout"
+import style from "./404.module.scss"
+
+const Custom404 = () => (
+  <Layout page="404">
+    <Head><title>404</title></Head>
+    <h1>Uh oh.</h1>
+    <Link href="/">
+      <a className={`btn ${style.btn}`}>Go back</a>
+    </Link>
+  </Layout>
+)
+
+export default Custom404
