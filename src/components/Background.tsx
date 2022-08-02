1025087445c82dd8b55cdcb0489e413c9452b1af
Tom Fletcher <rgbjoy@users.noreply.github.com>
2022-08-01 22:28:46 -0400

    Float, center, and mobile friendly

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -2,15 +2,15 @@ import { useState, useRef, useEffect } from 'react';
 
 import * as THREE from "three";
 import { Canvas, useFrame } from '@react-three/fiber'
-import { Points, PointMaterial } from '@react-three/drei'
-import { Bloom, EffectComposer, Noise } from "@react-three/postprocessing";
+import { Points, PointMaterial, Center, Float } from '@react-three/drei'
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
 import * as random from "maath/random";
 
 import gsap from "gsap"
 
 import style from "./Background.module.scss"
 
-function getRandomInt(min:any, max:any): number {
+function getRandomInt(min: any, max: any): number {
   return Math.random() * (max - min) + min;
 }
 
@@ -31,12 +31,16 @@ const Plane = (props: any) => {
   )
 }
 
-const Rotate = ({children}) => {
+const Rig = ({ children, page }) => {
   const ref = useRef(null)
   useFrame((state, delta) => {
+    let WIDTH = state.viewport.width * state.viewport.factor;
+    ref.current.position.y = WIDTH < 768 ? -0.65 : 0;
     ref.current.rotation.y += delta / 50;
   })
-  return <group ref={ref} {...children.props}></group>
+  return (
+    <group ref={ref} visible={page !== "/" ? false : true}>{children}</group>
+  )
 }
 
 const Stars = (props: any) => {
@@ -57,7 +61,7 @@ const Stars = (props: any) => {
 
 const Background = ({ page }) => {
   return (
-    <Canvas className={style.background} dpr={[1.5, 2]} camera={{ fov: 25, position: [5, 5, 5] }}
+    <Canvas className={style.background} camera={{ fov: 25, position: [5, 5, 5] }}
       gl={{
         powerPreference: "high-performance",
         alpha: false,
@@ -67,13 +71,15 @@ const Background = ({ page }) => {
       }}>
       <color attach="background" args={["#121212"]} />
       <Stars />
-      <Rotate>
-        <group visible={page !== "/" ? false : true}>
-          <Plane color="#FF0000" position={[-0.5, 0, 0]} rotation={[THREE.MathUtils.degToRad(90), 0, 0]} />
-          <Plane color="#0000FF" position={[0, 0, 0]} rotation={[0, THREE.MathUtils.degToRad(90), 0]} />
-          <Plane color="#00FF00" position={[-0.5, -0.5, 0]} rotation={[0, 0, 0]} />
-        </group>
-      </Rotate>
+      <Float>
+        <Center>
+          <Rig page={page} visible={page !== "/" ? false : true}>
+            <Plane color="#FF0000" position={[-0.5, 0, 0]} rotation={[THREE.MathUtils.degToRad(90), 0, 0]} />
+            <Plane color="#0000FF" position={[0, 0, 0]} rotation={[0, THREE.MathUtils.degToRad(90), 0]} />
+            <Plane color="#00FF00" position={[-0.5, -0.5, 0]} rotation={[0, 0, 0]} />
+          </Rig>
+        </Center>
+      </Float>
       <EffectComposer multisampling={3} disableNormalPass={true}>
         <Bloom kernelSize={3} height={300} width={300} luminanceThreshold={0} luminanceSmoothing={0.4} intensity={0.6} />
       </EffectComposer>
