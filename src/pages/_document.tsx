4ff3571b9cde6431e900b7cbc597127934c98f6e
Tom Fletcher <rgbjoy@users.noreply.github.com>
2022-08-02 14:46:16 -0400

    move to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,16 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/social/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/social/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/social/favicon-16x16.png"/>
+        <link rel="manifest" href="/social/site.webmanifest"/>
+        <link rel="mask-icon" href="/social/safari-pinned-tab.svg" color="#5bbad5"/>
+        <link rel="shortcut icon" href="/social/favicon.ico"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="msapplication-config" content="/social/browserconfig.xml"/>
+        <meta name="theme-color" content="#ffffff"/>
+
         {/* Global Site Tag (gtag.js) - Google Analytics */}
         <script
             async
