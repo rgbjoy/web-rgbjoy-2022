5df596696605ebb38eea83f951f11c7f591c4809
Tom Fletcher <rgbjoy@users.noreply.github.com>
2022-12-16 18:27:51 -0500

    Follow mouse

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,8 +12,8 @@ const Layout = ({ children, page }: Props) => {
     <>
       <motion.div
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { duration: 0.25, ease: [0, 0.71, 0.2, 1.01] } }}
-        exit={{ opacity: 0, transition: { duration: 0.25, ease: [0, 0.71, 0.2, 1.01]} }}
+        animate={{ opacity: 1, transition: { duration: 0.25, type: [0, 0.71, 0.2, 1.01] } }}
+        exit={{ opacity: 0, transition: { duration: 0.25, type: [0, 0.71, 0.2, 1.01]} }}
         className={`wrapper ${page ? page : ""}`}
       >
         {children}
--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -1,13 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Media from "../components/Media"
 import style from "../pages/art.module.scss"
+import gsap from "gsap"
 
 const LightBox = ({ children, media }) => {
+	const ref = useRef(null);
 	const [isOpen, setIsOpen] = useState(false);
 
 	const toggleIsOpen = () => {
 		setIsOpen(!isOpen);
 	};
+
+	const useMousePosition = () => {
+		const [position, setPosition] = useState({ x: 0, y: 0 });
+
+		useEffect(() => {
+			const setFromEvent = (e) => setPosition({ x: e.clientX, y: e.clientY });
+			window.addEventListener("mousemove", setFromEvent);
+
+			return () => {
+				window.removeEventListener("mousemove", setFromEvent);
+			};
+		}, []);
+
+		return position;
+	};
+
+	const position = useMousePosition();
+	if (isOpen && ref.current) {
+		gsap.to(ref.current, {y: (position.y-ref.current.clientHeight/2)*-1});
+	}
 
 	return (
 		<div onClick={toggleIsOpen}>
@@ -15,7 +37,7 @@ const LightBox = ({ children, media }) => {
 			{isOpen ?
 				<div onClick={toggleIsOpen} className={style.lightbox}>
 					<div className={style.lightbox__inner}>
-						<Media media={media} />
+						<div ref={ref}><Media media={media} /></div>
 						<div className={style.caption}>{media.title}</div>
 					</div>
 				</div>
