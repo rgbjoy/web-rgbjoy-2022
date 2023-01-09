30ed88a6ae9efef2c0e22759ac562751c1caff4f
Tom Fletcher <rgbjoy@users.noreply.github.com>
2023-01-09 16:40:13 -0500

    Next 13, image fixes

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -1,11 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Media from "../components/Media"
 import style from "../pages/art.module.scss"
-import gsap from "gsap"
-import useWindowDimensions from '../utils/useWindowDimensions'
 
 const LightBox = ({ children, media }) => {
-	const ref = useRef(null);
 	const [isOpen, setIsOpen] = useState(false);
 
 	const toggleIsOpen = () => {
@@ -19,32 +16,7 @@ const LightBox = ({ children, media }) => {
 			document.querySelector("#header").classList.remove("hidden")
 			document.querySelector("#footer").classList.remove("hidden")
 		}
-
 	};
-
-	const useMousePosition = () => {
-		const [position, setPosition] = useState({ x: 0, y: 0 });
-
-		useEffect(() => {
-			const setFromMouseEvent = (e) => setPosition({ x: e.clientX, y: e.clientY });
-			window.addEventListener("mousemove", setFromMouseEvent);
-
-			return () => {
-				window.removeEventListener("mousemove", setFromMouseEvent);
-			};
-		}, []);
-
-		return position;
-	};
-
-	let position = useMousePosition();
-	const { width } = useWindowDimensions();
-	if (isOpen && ref.current) {
-		if (width > 800) {
-			let posY = position.y - ref.current.offsetHeight /2
-			gsap.to(ref.current, {y: -posY} );
-		}
-	}
 
 	return (
 		<div onClick={toggleIsOpen}>
@@ -52,7 +24,7 @@ const LightBox = ({ children, media }) => {
 			{isOpen ?
 				<div onClick={toggleIsOpen} className={style.lightbox}>
 					<div className={style.lightbox__inner}>
-						<div className={style.lightbox_wrapper} ref={ref}><Media media={media} /></div>
+						<Media media={media} />
 						<div className={style.caption}>{media.title}</div>
 					</div>
 				</div>
--- src/components/Media.tsx
+++ src/components/Media.tsx
@@ -1,6 +1,21 @@
 import Image from 'next/image'
 
 const Media = ({media, thumbnail = null}) => {
+
+  const keyStr =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+
+  const triplet = (e1: number, e2: number, e3: number) =>
+    keyStr.charAt(e1 >> 2) +
+    keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+    keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+    keyStr.charAt(e3 & 63)
+
+  const rgbDataURL = (r: number, g: number, b: number) =>
+  `data:image/gif;base64,R0lGODlhAQABAPAA${
+    triplet(0, r, g) + triplet(b, 255, 255)
+  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`
+
   if (media.mediaType !== "image") {
     return (
       <video autoPlay loop width={thumbnail ? 300 : media.mediaDetails.width} height={thumbnail ? 300 : media.mediaDetails.height}>
@@ -15,10 +30,12 @@ const Media = ({media, thumbnail = null}) => {
         src={media.mediaItemUrl}
         width={media.mediaDetails.width}
         height={media.mediaDetails.height}
+        sizes="(max-width: 800px) 100vw"
+        placeholder="blur"
+        blurDataURL={rgbDataURL(90, 90, 90)}
         alt={media.title}
-        placeholder={"blur"}
-        blurDataURL={media.mediaDetails.sizes[0].sourceUrl}
         quality={thumbnail ? 75 : 100}
+        style={{maxWidth:media.mediaDetails.width, maxHeight:media.mediaDetails.height}}
         className={thumbnail ? "image" : "image-fill"}
       />
     )
