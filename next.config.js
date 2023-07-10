db9b0f7f6f403d557144705ccd9db9989f832284
Tom Fletcher <rgbjoy@users.noreply.github.com>
2023-07-10 15:08:07 -0400

    Bring this back

--- next.config.js
+++ next.config.js
@@ -14,6 +14,16 @@ const nextConfig = {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: `@import "./styles/variables.scss";`
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.pdf$/,
+      use: {
+        loader: 'file-loader',
+      },
+    });
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
