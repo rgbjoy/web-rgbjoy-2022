02f2ee2349c923477a539559e9497b258900e329
Tom Fletcher <rgbjoy@users.noreply.github.com>
2023-07-12 13:26:23 -0400

    actually add sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,19 +44,6 @@ export const metadata = {
       },
     ]
   },
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
 }
 
 export default function RootLayout({
