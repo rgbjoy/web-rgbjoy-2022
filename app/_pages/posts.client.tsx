708d500aae1152e0862fef6e785da90f0ef462bd
Tom Fletcher <rgbjoy@users.noreply.github.com>
2023-11-16 17:38:11 -0500

    bump

--- app/_pages/posts.client.tsx
+++ app/_pages/posts.client.tsx
@@ -6,6 +6,16 @@ import style from './posts.module.scss'
 import Link from 'next/link';
 
 export default function Posts({ posts }) {
+
+  const formatDate = (dateString) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    };
+    return new Intl.DateTimeFormat('en-US', options).format(new Date(dateString));
+  };
+
   return (
     <PageWrapper className={style.posts}>
       <h1 className={style.header}>
@@ -16,10 +26,10 @@ export default function Posts({ posts }) {
       {posts.nodes.map((node) => {
         return (
           <div key={node.slug}>
+            <p className={style.date}>{formatDate(node.date)}</p>
             <Link href={`/posts/${node.slug}`}>
               <div className={style.postLink} dangerouslySetInnerHTML={{ __html: node.title }} />
             </Link>
-            <p className={style.date}>{node.date}</p>
           </div>
         )
       })}
