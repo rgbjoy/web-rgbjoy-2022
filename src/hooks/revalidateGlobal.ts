d969d5093c9d8477f4a048221ad7a11053f44b35
Tom Fletcher <rgbjoy@users.noreply.github.com>
2024-11-24 12:46:29 -0500

    shot in the dark

--- src/hooks/revalidateGlobal.ts
+++ src/hooks/revalidateGlobal.ts
@@ -1,14 +1,18 @@
 import type { GlobalAfterChangeHook } from 'payload'
-import { revalidateTag } from 'next/cache'
+import { revalidatePath } from 'next/cache'
 
 export const revalidateGlobal: GlobalAfterChangeHook = ({
   doc,
   req: { payload, url },
 }) => {
-  const globalSlug = url?.split('/globals/')?.[1]?.split('?')?.[0] || doc.globalType || ''
+  const globalSlug = url?.split('/globals/')?.[1]?.split('?')?.[0] || ''
   payload.logger.info(`Revalidating global: ${globalSlug}`)
 
-  revalidateTag(globalSlug)
+  if (globalSlug === 'home') {
+    revalidatePath('/')
+  } else {
+    revalidatePath(`/${globalSlug}`)
+  }
 
   return doc
 }
