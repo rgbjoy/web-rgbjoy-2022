88bc467ce12b32e3915079af3f6a177f6ef025c7
Tom Fletcher <rgbjoy@users.noreply.github.com>
2024-12-10 17:41:23 -0500

    revalidation logging

--- src/hooks/revalidateGlobal.ts
+++ src/hooks/revalidateGlobal.ts
@@ -3,7 +3,7 @@ import { revalidatePath } from 'next/cache'
 
 export const revalidateGlobal: GlobalAfterChangeHook = ({ doc, req: { payload, url } }) => {
   const globalSlug = url?.split('/globals/')?.[1]?.split('?')?.[0] || ''
-  payload.logger.info(`Revalidating global: ${globalSlug}`)
+  payload.logger.info(`Revalidating global: ${url} ${globalSlug}`)
 
   if (globalSlug === 'home') {
     revalidatePath('/')
