8984b87eb4543cdf13e34f20dc6a7739d944b487
Tom Fletcher <rgbjoy@users.noreply.github.com>
2024-12-10 20:56:18 -0500

    trying whatever

--- src/hooks/revalidateGlobal.ts
+++ src/hooks/revalidateGlobal.ts
@@ -2,9 +2,9 @@ import type { GlobalAfterChangeHook } from 'payload'
 
 import { revalidateTag } from 'next/cache'
 
-export const revalidateGlobal: GlobalAfterChangeHook = ({ doc, req: { payload, context } }) => {
+export const revalidateGlobal: GlobalAfterChangeHook = ({ doc, req: { payload, context }, }) => {
   if (!context.disableRevalidate) {
-    payload.logger.info(`Revalidating ${doc.slug}`)
+    payload.logger.info(`Revalidating ${doc.slug} ${doc} ${context.url} ${context.globalSlug}`)
 
     revalidateTag(`global_${doc.slug}`)
   }
