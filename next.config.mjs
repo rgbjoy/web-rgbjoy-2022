2aab06976e10a72f1bd75c048bee14103b3f7085
Tom Fletcher <rgbjoy@users.noreply.github.com>
2024-12-11 14:47:55 -0500

    trying this

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   expireTime: 3600,
   experimental: {
